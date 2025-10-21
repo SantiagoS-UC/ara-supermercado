38471d918ba6aa4417d58f969ec0294df9aa1ea7
SantiagoS-UC <santiagosolarte@unicauca.edu.co>
2025-10-21 12:47:02 -0500

    Se agrega la estructura de los formularios de usuario

---
+++ AraSupermercado/presentacion/FormLogin.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AraSupermercado.presentacion
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlIniciarSesion = new System.Windows.Forms.Panel();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblIniciarSesion = new System.Windows.Forms.Label();
+            this.lblNoTienesCuenta = new System.Windows.Forms.Label();
+            this.pnlIniciarSesion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlIniciarSesion
+            // 
+            this.pnlIniciarSesion.Controls.Add(this.lblNoTienesCuenta);
+            this.pnlIniciarSesion.Controls.Add(this.btnRegistrarse);
+            this.pnlIniciarSesion.Controls.Add(this.btnIniciarSesion);
+            this.pnlIniciarSesion.Controls.Add(this.txtContrasena);
+            this.pnlIniciarSesion.Controls.Add(this.txtCorreo);
+            this.pnlIniciarSesion.Controls.Add(this.lblContrasena);
+            this.pnlIniciarSesion.Controls.Add(this.lblCorreo);
+            this.pnlIniciarSesion.Controls.Add(this.lblIniciarSesion);
+            this.pnlIniciarSesion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlIniciarSesion.Location = new System.Drawing.Point(0, 0);
+            this.pnlIniciarSesion.Name = "pnlIniciarSesion";
+            this.pnlIniciarSesion.Size = new System.Drawing.Size(878, 430);
+            this.pnlIniciarSesion.TabIndex = 0;
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.Location = new System.Drawing.Point(367, 304);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(127, 29);
+            this.btnRegistrarse.TabIndex = 6;
+            this.btnRegistrarse.Text = "Registrarse";
+            this.btnRegistrarse.UseVisualStyleBackColor = true;
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.Location = new System.Drawing.Point(367, 226);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(127, 31);
+            this.btnIniciarSesion.TabIndex = 5;
+            this.btnIniciarSesion.Text = "Iniciar Sesión";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(328, 180);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(239, 22);
+            this.txtContrasena.TabIndex = 4;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(328, 118);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(239, 22);
+            this.txtCorreo.TabIndex = 3;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Location = new System.Drawing.Point(325, 160);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(76, 16);
+            this.lblContrasena.TabIndex = 2;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(325, 99);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(118, 16);
+            this.lblCorreo.TabIndex = 1;
+            this.lblCorreo.Text = "Correo Electrónico";
+            // 
+            // lblIniciarSesion
+            // 
+            this.lblIniciarSesion.AutoSize = true;
+            this.lblIniciarSesion.Location = new System.Drawing.Point(407, 9);
+            this.lblIniciarSesion.Name = "lblIniciarSesion";
+            this.lblIniciarSesion.Size = new System.Drawing.Size(87, 16);
+            this.lblIniciarSesion.TabIndex = 0;
+            this.lblIniciarSesion.Text = "Iniciar Sesión";
+            // 
+            // lblNoTienesCuenta
+            // 
+            this.lblNoTienesCuenta.AutoSize = true;
+            this.lblNoTienesCuenta.Location = new System.Drawing.Point(373, 271);
+            this.lblNoTienesCuenta.Name = "lblNoTienesCuenta";
+            this.lblNoTienesCuenta.Size = new System.Drawing.Size(121, 16);
+            this.lblNoTienesCuenta.TabIndex = 7;
+            this.lblNoTienesCuenta.Text = "¿No tienes cuenta?";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 430);
+            this.Controls.Add(this.pnlIniciarSesion);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.pnlIniciarSesion.ResumeLayout(false);
+            this.pnlIniciarSesion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlIniciarSesion;
+        private System.Windows.Forms.Label lblIniciarSesion;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Button btnRegistrarse;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label lblNoTienesCuenta;
+    }
+}
