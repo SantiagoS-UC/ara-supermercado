14f15ef87502e9d560994568377cd16c6880c2cf
GinnerOrtega <ginnerortega@unicauca.edu.co>
2025-10-22 07:50:40 -0500

    Form Registrar producto final

--- AraSupermercado/presentacion/FormRegistrarProducto.cs
+++ AraSupermercado/presentacion/FormRegistrarProducto.cs
@@ -16,5 +16,32 @@ namespace AraSupermercado.presentacion
         {
             InitializeComponent();
         }
+
+        private void picProducto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Title = "Seleccionar imagen del producto";
+            abrir.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                picProducto.Image = Image.FromFile(abrir.FileName);
+                lblSeleccionarImagen.Visible = false; //Oculta el texto después de cargar la imagen
+            }
+        }
+
+        private void FormRegistrarProducto_Load(object sender, EventArgs e)
+        {
+            ToolTip tip = new ToolTip();
+            tip.SetToolTip(picProducto, "Haz clic aquí para subir una imagen");
+
+            picProducto.Cursor = Cursors.Hand;
+            lblSeleccionarImagen.Cursor = Cursors.Hand;//Cursor de mano
+        }
+
+        private void lblSeleccionarImagen_Click(object sender, EventArgs e)
+        {
+            picProducto_Click(sender, e);
+        }
     }
 }
